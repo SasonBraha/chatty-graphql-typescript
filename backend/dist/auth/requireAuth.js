26e98a9aebe5ed87c59a72452f944b39e568bf39
Sason Braha <sasonbraha@gmail.com>
2019-03-16 12:35:26 +0200

    Removed ignored files

--- backend/dist/auth/requireAuth.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const passport = require("passport");
-exports.default = passport.authenticate('jwt', { session: false });
--- backend/dist/entities/File/file.typeDefs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const apollo_server_express_1 = require("apollo-server-express");
-exports.default = apollo_server_express_1.gql `
-	type File {
-		type: String!
-		link: String1
-	}
-`;
